d0ad3fcf60897c77fdfc80302d1301d91e100b16
Oliveira-Jovs <joaoluca2909@gmail.com>
2024-10-21 17:44:48 -0300

    Inserção para realização de criar evento

---
+++ BackEnd/src/main/java/br/edu/ifpe/gestaoacademica/entities/Evento.java
@@ -0,0 +1,33 @@
+package br.edu.ifpe.gestaoacademica.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "Evento")
+@Entity(name = "eventos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Evento {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String descricao;
+	private String data;
+	private String local;
+	private String tipo;
+	private boolean ativo;
+	
+	private Long id_servidor;
+}
---
+++ BackEnd/src/main/java/br/edu/ifpe/gestaoacademica/service/EventoService.java
@@ -0,0 +1,36 @@
+package br.edu.ifpe.gestaoacademica.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import br.edu.ifpe.gestaoacademica.controllers.dto.CadastrarEventoDTO;
+import br.edu.ifpe.gestaoacademica.entities.Evento;
+import br.edu.ifpe.gestaoacademica.repository.EventoRepository;
+@CrossOrigin(origins = "http://localhost:3000")
+
+@Service
+public class EventoService {
+	
+	@Autowired
+	private EventoRepository eventoRepository;
+	
+	public Evento cadastrarEvento(CadastrarEventoDTO dadosEventoDTO ) {
+		
+        Long idServidorFixo = 1L;
+        
+		Evento evento = new Evento();
+		evento.setNome(dadosEventoDTO.nome());
+		evento.setDescricao(dadosEventoDTO.descricao());
+		evento.setData (dadosEventoDTO.data());
+		evento.setLocal (dadosEventoDTO.local());
+		evento.setTipo (dadosEventoDTO.tipo());
+		evento.setAtivo (true);
+		
+		evento.setId_servidor(idServidorFixo);
+		
+		return eventoRepository.save(evento);
+		
+	}
+
+}
